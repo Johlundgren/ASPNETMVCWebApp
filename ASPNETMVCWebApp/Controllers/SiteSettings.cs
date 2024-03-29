ecb704d058479eb1a69935880807c8b7783828af
Johan <gunbald@live.se>
2024-03-29 18:46:37 +0100

    la till dark mode o sånt men funkar inte.

---
+++ ASPNETMVCWebApp/Controllers/SiteSettings.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ASPNETMVCWebApp.Controllers;
+
+public class SiteSettings : Controller
+{
+    public IActionResult ChangeTheme(string mode)
+    {
+        var option = new CookieOptions
+        {
+            Expires = DateTime.Now.AddDays(60),
+        };
+        Response.Cookies.Append("ThemeMode", mode, option);
+        return Ok();
+    }
+
+
+    [HttpPost]
+    public IActionResult CookieConsent()
+    {
+        var option = new CookieOptions
+        {
+            Expires = DateTime.Now.AddYears(1),
+            HttpOnly = true,
+            Secure = true
+        };
+        Response.Cookies.Append("CookieConsent", "true", option);
+        return Ok();
+    }
+}
--- ASPNETMVCWebApp/wwwroot/js/site.js
+++ ASPNETMVCWebApp/wwwroot/js/site.js
@@ -4,3 +4,23 @@ mobileBtn.addEventListener('click', () => {
     const menu = document.querySelector('#mobile-menu')
     menu.classList.toggle('open')
 })
+
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    let sw = document.querySelector('#switch-mode')
+
+    sw.addEventListener('change', function () {
+        let theme = this.checked ? "dark" : "light"
+
+
+        fetch(`/sitesettings/changetheme?mode=${theme}`)
+            .then(res => {
+                if (res.ok)
+                    window.location.reload()
+                else 
+                console.log('something')
+            })
+
+    })
+})

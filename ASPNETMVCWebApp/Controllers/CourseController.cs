fbaf09c98d734274d9d417f34ff60824ff66d34b
Johan <gunbald@live.se>
2024-04-05 13:38:12 +0200

    Allt fungerar korrekt med searchquery och kategorier.

--- ASPNETMVCWebApp/Controllers/CourseController.cs
+++ ASPNETMVCWebApp/Controllers/CourseController.cs
@@ -15,7 +15,7 @@ public class CourseController(HttpClient http, IConfiguration configuration) : C
     private readonly HttpClient _http = http;
     private readonly IConfiguration _configuration = configuration;
 
-    public async Task<IActionResult> Courses()
+    public async Task<IActionResult> Courses(string category = "", string searchQuery = "")
     {
         var viewModel = new CourseViewModel();
 
@@ -23,21 +23,40 @@ public class CourseController(HttpClient http, IConfiguration configuration) : C
         {
             _http.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
 
-            var response = await _http.GetAsync($"https://localhost:7226/api/courses?key={_configuration["ApiKey:Secret"]}");
-            if (response.IsSuccessStatusCode)
+            //Url grejer
+            string courseUrl = $"https://localhost:7226/api/courses?key={_configuration["ApiKey:Secret"]}";
+            if (!string.IsNullOrEmpty(category))
             {
-                viewModel = JsonConvert.DeserializeObject<CourseViewModel>(await response.Content.ReadAsStringAsync());
-                if (viewModel != null && viewModel.Succeeded)
-                { 
-                    return View(viewModel);
-                }
-                //var courses = JsonConvert.DeserializeObject<IEnumerable<CourseEntity>>(await response.Content.ReadAsStringAsync());
-               
-                //return View(courses);
+                courseUrl += $"&category={Uri.EscapeDataString(category)}";
+            }
+
+            if (!string.IsNullOrEmpty(searchQuery))
+            {
+                courseUrl += $"&searchQuery={Uri.EscapeDataString(searchQuery)}";
+            }
+
+            // Kurser
+            var courseResponse = await _http.GetAsync(courseUrl);
+            if (courseResponse.IsSuccessStatusCode)
+            {
+                viewModel = JsonConvert.DeserializeObject<CourseViewModel>(await courseResponse.Content.ReadAsStringAsync());
+            }
+
+            // Kategorier
+            var categoryResponse = await _http.GetAsync($"https://localhost:7226/api/categories?key={_configuration["ApiKey:Secret"]}");
+            if (categoryResponse.IsSuccessStatusCode)
+            {
+                var categories = JsonConvert.DeserializeObject<IEnumerable<Category>>(await categoryResponse.Content.ReadAsStringAsync());
+                viewModel.Categories = categories;
+            }
+
+            if (viewModel != null && viewModel.Succeeded)
+            {
+                return View(viewModel);
             }
         }
 
-        return View();
+        return View(viewModel);
     }
 
     public async Task<IActionResult> Details(int id)
@@ -57,3 +76,26 @@ public class CourseController(HttpClient http, IConfiguration configuration) : C
         return View();
     }
 }
+
+//public async Task<IActionResult> Courses()
+//{
+//    var viewModel = new CourseViewModel();
+
+//    if (HttpContext.Request.Cookies.TryGetValue("AccessToken", out var token))
+//    {
+//        _http.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+//        var response = await _http.GetAsync($"https://localhost:7226/api/courses?key={_configuration["ApiKey:Secret"]}");
+//        if (response.IsSuccessStatusCode)
+//        {
+//            viewModel = JsonConvert.DeserializeObject<CourseViewModel>(await response.Content.ReadAsStringAsync());
+
+//            if (viewModel != null && viewModel.Succeeded)
+//            {
+//                return View(viewModel);
+//            }
+//        }
+//    }
+
+//    return View();
+//}
---
+++ Infrastructure/Services/CourseService.cs
@@ -0,0 +1,25 @@
+﻿
+using Infrastructure.Models;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+
+namespace Infrastructure.Services;
+
+    public class CourseService(HttpClient http, IConfiguration configuration)
+    {
+        private readonly HttpClient _http = http;
+        private readonly IConfiguration _configuration = configuration;
+
+
+        public async Task<IEnumerable<Course>> GetCoursesAsync()
+        {
+            var response = await _http.GetAsync($"https://localhost:7226/api/courses?key={_configuration["ApiKey:Secret"]}");
+            if (response.IsSuccessStatusCode)
+            {
+                var courses = JsonConvert.DeserializeObject<IEnumerable<Course>>(await response.Content.ReadAsStringAsync());
+                return courses ??= null!;
+            }
+            return null!;
+        }
+    }
+

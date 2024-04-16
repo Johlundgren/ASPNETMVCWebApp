144e7d99decc752161f0df7e8bbc279116a96faf
Johan <gunbald@live.se>
2024-04-16 12:13:35 +0200

    La till contact controller

---
+++ ASPNETMVCWebApp/Controllers/ContactController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ASPNETMVCWebApp.Controllers;
+
+public class ContactController : Controller
+{
+    public IActionResult ContactIndex()
+    {
+        return View();
+    }
+}

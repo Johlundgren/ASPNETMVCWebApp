38e2035717b3d3bed94caf8ad6f1d1c5059f319c
Johan <gunbald@live.se>
2024-04-16 16:29:05 +0200

    Delvis fixat contact

---
+++ ASPNETMVCWebApp/ViewModels/ContactRequestViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ASPNETMVCWebApp.ViewModels;
+
+public class ContactRequestViewModel
+{
+    [Required(ErrorMessage = "A valid first name is required")]
+    [DataType(DataType.Text)]
+    [Display(Name = "First name", Prompt = "Enter your first name")]
+    public string FullName { get; set; } = null!;
+
+
+    [Required(ErrorMessage = "A valid email is required")]
+    [DataType(DataType.EmailAddress)]
+    [Display(Name = "Email address", Prompt = "Enter your email address")]
+    public string Email { get; set; } = null!;
+
+    [DataType(DataType.MultilineText)]
+    [Display(Name = "Message", Prompt = "Enter your message here...")]
+    public string? Message { get; set; }
+}
